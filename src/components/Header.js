351de982b2bdda389652fc4ca70b52ab511d7b44
Elsa Jansen <elsajansen@hotmail.com>
2023-10-12 17:06:15 +0200

    added logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
+import "../styles/App.css";
+
 export const Header = () => {
   return (
     <>
       <div className="header-text">
+        {" "}
+        <img
+          className="logo"
+          src="https://raw.githubusercontent.com/ElsaJ88/DigitalAssetManagement/main/src/assets/logo3.png"
+          alt="Subway Logo"
+        />
         <div className="color1">Digital</div>
-        <div className="color2">Assets</div>
-      </div>
+        <div className="color2">Assets</div>{" "}
+      </div>{" "}
+      <img
+        className="rgn"
+        src="https://www.rgn.nl/wp-content/uploads/2022/08/Group-1.svg"
+        alt="RGN Logo"
+      />
     </>
   );
 };
