6e55784c79b51f1092451340ce1e0c52ad5d3475
Elsa Jansen <elsajansen@hotmail.com>
2023-10-12 17:57:12 +0200

    changed logo position

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,22 @@ import "../styles/App.css";
 export const Header = () => {
   return (
     <>
-      <div className="header-text">
-        {" "}
+      <div className="logo-container">
+        <img
+          className="rgn"
+          src="https://www.rgn.nl/wp-content/uploads/2022/08/Group-1.svg"
+          alt="RGN Logo"
+        />
         <img
           className="logo"
           src="https://raw.githubusercontent.com/ElsaJ88/DigitalAssetManagement/main/src/assets/logo3.png"
           alt="Subway Logo"
         />
+      </div>
+      <div className="header-text">
         <div className="color1">Digital</div>
         <div className="color2">Assets</div>{" "}
       </div>{" "}
-      <img
-        className="rgn"
-        src="https://www.rgn.nl/wp-content/uploads/2022/08/Group-1.svg"
-        alt="RGN Logo"
-      />
     </>
   );
 };
